a00bbf452ebaf0b238968ed5adca62cf03071ec4
arthurtuio <arthur.henrique.2709@gmail.com>
2021-06-17 14:41:45 -0300

    adding first validator

---
+++ scripts/yml_validator.py
@@ -0,0 +1,44 @@
+import yaml
+from cerberus import Validator
+
+
+def default_params_base():
+    return {
+        'dag_name': {'type': 'string'},
+        'owner': {'type': 'string'},
+        'start_date': {'type': 'string'},
+        'schedule': {'type': 'string'},
+    }
+
+
+def task_params_base():
+    return {
+        'tasks': {
+            'type': 'dict',
+            'schema': {
+                'name': {'type': 'string'},
+                'source': {'type': 'string'},
+                'destination': {'type': 'string'},
+                'source_sql': {'type': 'string'},
+                'destination_sql': {'type': 'boolean'},
+            }
+        }
+    }
+
+
+def validate(obj_to_validate):
+    validator_obj = Validator(
+        {
+            **default_params_base(),
+            **task_params_base(),
+        }
+    )
+    return validator_obj(obj_to_validate)  # vai retornar True ou False
+
+
+if __name__ == '__main__':
+    yaml_file = open("/home/arthur/clone_airflow_yml/airflow-yml/contexts/marketing_example/example_dag.yml")
+    obj = yaml.load(yaml_file, Loader=yaml.FullLoader)
+    # print(obj)
+
+    print(validate(obj))
