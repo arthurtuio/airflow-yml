4389da4293656b90607bc0a74b96752ea51ca418
arthurtuio <arthur.henrique.2709@gmail.com>
2021-06-21 19:38:31 -0300

    a

--- tests/test_using_cerberus.py
+++ tests/test_using_cerberus.py
@@ -1,9 +1,35 @@
+import os
 import yaml
+
 from yml_validator import validate
 
 
-def test_validation_using_pytest():
-    yaml_file = open("/home/arthur/clone_airflow_yml/airflow-yml/contexts/marketing_example/example_send_data_from_bq_to_hubspot.yml")
-    obj = yaml.load(yaml_file, Loader=yaml.FullLoader)
+BASE_REPO_DIR = os.path.normpath(os.getcwd() + os.sep + os.pardir)
+CONTEXTS_DIR = BASE_REPO_DIR + "/contexts"
 
-    assert validate(obj) == True
+
+def get_all_contexts():
+    files_list = []
+
+    for subdir, dirs, files in os.walk(CONTEXTS_DIR):
+        for file in files:
+            # print(os.path.join(subdir, file))
+            if not file.endswith(".md"):
+                files_list.append(os.path.join(subdir, file))
+
+    # print(files_list)
+    return files_list
+
+
+def test_validation_using_pytest():
+    for yaml_file in get_all_contexts():
+        obj = yaml.load(open(yaml_file), Loader=yaml.FullLoader)
+        print(obj)
+        print(type(obj))
+
+        assert validate(obj) == True
+
+
+if __name__ == '__main__':
+    get_all_contexts()
+    test_validation_using_pytest()
